1925c09ce594cf5db53566f17bcbd43b043a2ea4
Ryan Gilbert <ryangilbert7926@gmail.com>
2022-12-07 21:28:48 -0500

    feat: basic dynamic wagmi setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { DynamicContextProvider } from '@dynamic-labs/sdk-react';
+import { DynamicWagmiConnector } from '@dynamic-labs/wagmi-connector';
+import { configureChains, createClient, mainnet, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { provider } = configureChains([mainnet], [publicProvider()]);
+
+const client = createClient({
+  provider,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <DynamicContextProvider
+      settings={{
+        environmentId: '1a5bc82b-cca0-497e-9481-036d5821e14e',
+      }}
+    >
+      <WagmiConfig client={client}>
+        <DynamicWagmiConnector>
+          <Component {...pageProps} />
+        </DynamicWagmiConnector>
+      </WagmiConfig>
+    </DynamicContextProvider>
+  );
 }
